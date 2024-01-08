5ea54df0608ad4fd4a8b398005aa124bd00348ff
anapppp <anapaula.ksoares@gmail.com>
2024-01-08 13:35:32 -0300

    engine implemented

---
+++ src/scripts/engine.js
@@ -0,0 +1,11 @@
+const emojis = ["🧜🏻‍♂️", "🧜🏻‍♂️", "🔮", "🔮", "🪅", "🪅", "🗿", "🗿", "🪂", "🪂", "🧻", "🧻"];
+let openCards = [];
+
+let shuffleEmojis = emojis.sort(() => (Math.random() > 0.5 ? 2 : -1))
+
+for (let i = 0; i < emojis.length; i++) {
+    let box = document.createElement("div");
+    box.className = "item";
+    box.innerHTML = shuffleEmojis[i];
+    document.querySelector(".game").appendChild(box);
+}
